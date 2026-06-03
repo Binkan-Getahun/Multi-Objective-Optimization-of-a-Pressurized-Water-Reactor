666dec6078ccaa1c8304238fd6a24fb01e0106d8
Binkan <binkeneang@gmail.com>
2026-06-03 08:17:47 +0200

    Update heater.py

--- components/heater.py
+++ components/heater.py
@@ -1,10 +1,7 @@
-from iapws import IAPWS97
+def get_fwh_coefficients(extraction_h, drain_out_h, fw_in_h, fw_out_h):
+    """Returns energy balance coefficients for a matrix."""
 
-
-
-def get_state(name, **kwargs):
-    """Computes all thermodynamic properties for a given state."""
-   
-    state = IAPWS97(**kwargs)
+    extract_coeff = extraction_h - drain_out_h
+    fw_coeff = fw_in_h - fw_out_h
     
-    return state
+    return extract_coeff, fw_coeff
